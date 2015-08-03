857c16d56e6c6e07aabd432fe7fad1fae833ed87
Krishna Suraj <krishnasuraj.lhs@gmail.com>
2015-08-03 06:35:05 -0400

    created TreeNode file
    Changing representation of tree so that we can target specific nodes
    (so it’s less confusing)

---
+++ TreeNode.py
@@ -0,0 +1,17 @@
+import math
+import random
+
+class TreeNode:
+
+	def __init__(self):
+		self._left=None
+		self._right=None
+
+	def getLeft(self, node):
+		return node._left;
+
+	def getRight(self, node):
+		return node._right;
+
+
+	def merge
