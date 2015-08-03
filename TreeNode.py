4dbf5d4e0988df34009cade439d4ba94ae12255f
Krishna Suraj <krishnasuraj.lhs@gmail.com>
2015-08-03 06:45:57 -0400

    comments for clarity

--- TreeNode.py
+++ TreeNode.py
@@ -1,12 +1,16 @@
 import math
 import random
 
+#Tree and associated methods
+#zero = 0, noisy = -1, real = 1
+
 class TreeNode:
 
 	def __init__(self):
 		self._left=None
 		self._right=None
 		self._data = []
+		self._metadata=[] #corresponds to _data, tells each piece of data where to go
 
 	def getLeft(self, node):
 		return node._left;
