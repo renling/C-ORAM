e82615630a323a061c6b17d4095cc1bdc358b809
Ling Ren <renling8@gmail.com>
2016-10-04 11:55:24 -0400

    change the attack to 1 real + A-1 noisy

--- Break.py
+++ Break.py
@@ -1,32 +1,36 @@
+#!/usr/bin/python
+
 from Tree import Tree
 import math
 import random
 
-
-def ForceDupPerm(attack):
+def ForceDupPerm(pattern):
 	ORAMsize = (1 << 3) - 1
-	Z = 40
+	Z = 60
 	A = 20
 	tree = Tree(ORAMsize, Z)
 
-	DummyRoot = [0] * A
+	DummyRoot = [tree.randomLeaf()] + [-1] * (A-1)
+	#DummyRoot = [0] * (A)
 	
 	# eviction 1
 	root = [tree.randomLeaf() for i in range(A)] 
-	if attack == False:
+	if pattern[0] == False:
 		root = DummyRoot       
 	ret = tree.evictAll(root)
 	Pi1, parent1, child1 = ret[0][0], ret[0][1], ret[0][2]
 
 	# eviction 2
 	root = [tree.randomLeaf() for i in range(A)]        
-	if attack == False:
+	if pattern[1] == False:
 		root = DummyRoot       
 	ret = tree.evictAll(root)
 	Pi2, parent2, child2 = ret[0][0], ret[0][1], ret[0][2]
 
 	# eviction 3
-	root = DummyRoot  	# keep doing dummy accesses for this epoch        
+	root = [tree.randomLeaf() for i in range(A)]        
+	if pattern[2] == False:
+		root = DummyRoot  	
 	ret = tree.evictAll(root)
 	Pi3, parent3, child3 = ret[1][0], ret[1][1], ret[1][2]
 
@@ -49,13 +53,17 @@ def breakCORAM():
 	numTest = 1000
 	dup1 = 0
 	for k in range(numTest):
-		dup1 += ForceDupPerm(True)
+		dup1 += ForceDupPerm([True,True,True])
 
 	dup2 = 0
 	for k in range(numTest):
-		dup2 += ForceDupPerm(False)
+		dup2 += ForceDupPerm([True,True,False])
 
-	print(dup1, dup2)
+	dup3 = 0
+	for k in range(numTest):
+		dup3 += ForceDupPerm([False,False,False])
+
+	print(dup1, dup2, dup3)
 
 	
 breakCORAM()	
